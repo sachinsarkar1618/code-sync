50693cb4c3a60b610c50f5db43c84cd81a16b936
sachin <sarkarsachindak@gmail.com>
2025-06-28 18:45:20 +0530

    making the page.jsx file in the app branch

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import InputFormHomePage from '@/components/InputFormHomePage'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <InputFormHomePage/>
+    </div>
+  )
+}
+
+export default Home
